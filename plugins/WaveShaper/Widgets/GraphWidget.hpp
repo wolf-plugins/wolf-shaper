af1dbf6f7950284a6e999bbc752687bfc953e06a
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2018-02-04 19:21:58 -0500

    Document GraphWidget header

--- plugins/WaveShaper/Widgets/GraphWidget.hpp
+++ plugins/WaveShaper/Widgets/GraphWidget.hpp
@@ -24,53 +24,130 @@ public:
   GraphWidget(WaveShaperUI *ui, Window &parent);
   ~GraphWidget();
 
+  /**
+   * Recreate the graph according to a saved state.
+   */
   void rebuildFromString(const char *serializedGraph);
+
+  /**
+   * Reset the graph back into its default state.
+   */
   void reset();
 
 protected:
+  /**
+   * DPF stuff
+   */
   void onNanoDisplay() override;
   bool onScroll(const ScrollEvent &ev) override;
   bool onMouse(const MouseEvent &ev) override;
   bool onMotion(const MotionEvent &ev) override;
 
+  /**
+   * Unused.
+   */
   void updateAnimations();
-  void flipYAxis();
-  void drawSubGrid();
-  void drawGrid();
-  void drawBackground();
-  void drawGraphLine(float lineWidth, Color color);
-  void drawAlignmentLines();
-  void positionTensionHandles();
 
   /**
-   * Insert a new vertex into the graph and return a pointer to it. 
+   * Flip the Y axis so that the origin of the graph is located at the bottom-left corner.
+   */
+  void flipYAxis();
+
+  /**
+   * Draw the grid of the graph.
+   */
+  void drawGrid();
+
+  /**
+   * Divide the cells on the grid into more cells.
+   */
+  void drawSubGrid();
+
+  /**
+   * Draw the background of the graph.
+   */
+  void drawBackground();
+
+  /**
+   * Draw the line on the graph according to the current state of the plugin.
+   */
+  void drawGraphLine(float lineWidth, Color color);
+
+  /**
+   * Draw lines to help the user align the currently grabbed node.
+   */
+  void drawAlignmentLines();
+
+  /**
+   * Insert a new vertex into the graph at a specified location and return a pointer to it. 
+   * The position is in absolute coordinates.
    */
   GraphVertex *insertVertex(const DGL::Point<int> pos);
+
+  /**
+   * Remove a vertex at the specified index.
+   */
   void removeVertex(const int index);
 
+  /**
+   * Mouse handling stuff
+   */
   bool leftClick(const MouseEvent &ev);
   bool middleClick(const MouseEvent &ev);
   bool rightClick(const MouseEvent &ev);
 
 private:
-  void initializeDefaultVertices();
-  void resetVerticesPool();
-
-  spoonie::Graph lineEditor;
-
   WaveShaperUI *ui;
 
+  /**
+   * Initialize the left and right vertices in the graph.
+   */
+  void initializeDefaultVertices();
+
+  /**
+   * Reset the object pool that contains the vertices of the graph.
+   */
+  void resetVerticesPool();
+
+  /**
+   * The data structure that contains the graph. Kept synchronized with the dsp side of the plugin.
+   */
+  spoonie::Graph lineEditor;
+
+  /**
+   * The widget on which the nodes are rendered.
+   */
   GraphNodesLayer graphNodesLayer;
+
+  /**
+   * Contains the vertex widgets used in the graph.
+   */
   GraphVertex *graphVertices[spoonie::maxVertices];
+
+  /**
+   * Object pool that holds the vertex widgets.
+   */
   spoonie::ObjectPool<GraphVertex> graphVerticesPool;
 
+  /**
+   * The graph element which currently has mouse focus.
+   */
   GraphNode *focusedElement;
 
+  /**
+   * Determine if a mouse button is down.
+   */
   bool mouseLeftDown;
   bool mouseRightDown;
 
+  /**
+   * The radius size of the vertices in the graph.
+   */
   const float absoluteVertexSize = 7.0f;
 
+  /**
+   * Define the space around the grid of the graph.
+   */
   const int marginTop = 48;
   const int marginLeft = 36;
   const int marginRight = 36;
