17f5ab75bfc6a5f788c0d23259be58a1c6b3f281
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2018-03-05 18:58:23 -0500

    Fix blank oversampling rectangle on Windows and Mac

--- plugins/WaveShaper/Widgets/src/OversampleWheel.cpp
+++ plugins/WaveShaper/Widgets/src/OversampleWheel.cpp
@@ -12,38 +12,38 @@ OversampleWheel::OversampleWheel(NanoWidget *widget, Size<uint> size) noexcept :
     loadSharedResources();
 }
 
-void OversampleWheel::draw() 
+void OversampleWheel::draw()
 {
     const float width = getWidth();
     const float height = getHeight();
 
-    const char* oversamplingFactors[] = {"", "2x", "4x", "8x", "16x"};
+    const char *oversamplingFactors[] = {"", "2x", "4x", "8x", "16x"};
 
     resetTransform();
 
     beginPath();
 
-    fillColor(Color(255,0,0,255));
+    fillColor(Color(255, 0, 0, 255));
     rect(0, 0, width, height);
     fill();
 
     closePath();
 
-    beginPath();
-    
-    fontFace(NANOVG_DEJAVU_SANS_TTF);
-    fontSize(height);
-    fillColor(Color(255,255,255, 255));
-    textAlign(ALIGN_RIGHT | ALIGN_BOTTOM);
-
     const int value = getValue();
 
-    if(value > 0)
+    if (value > 0)
+    {
+        beginPath();
+
+        fontFace(NANOVG_DEJAVU_SANS_TTF);
+        fontSize(height);
+        fillColor(Color(255, 255, 255, 255));
+        textAlign(ALIGN_RIGHT | ALIGN_BOTTOM);
+
         text(width - 2, height + 1, oversamplingFactors[getValue()], NULL);
 
-    fill();
-
-    closePath();
+        closePath();
+    }
 }
 
 END_NAMESPACE_DISTRHO
