bf8326e02c244137612b80061772fd917749c16e
Patrick Desaulniers <desaulniers.patrick@carrefour.cegepvicto.ca>
2018-11-12 20:59:52 -0500

    Add script for creating source tarball that includes submodules

---
+++ bundle_source.sh
@@ -0,0 +1,11 @@
+PLUGIN_NAME=wolf-shaper
+PLUGIN_VERSION=0.1.6
+
+git clone --recursive "https://github.com/pdesaulniers/$PLUGIN_NAME.git"
+cd "$PLUGIN_NAME"
+find . -name ".git" -type f -delete
+rm -rf .git
+find . -name ".gitmodules" -type f -delete
+cd ..
+tar -zcvf "$PLUGIN_NAME-v$PLUGIN_VERSION-source.tar.gz" "$PLUGIN_NAME/"
+rm -rf "$PLUGIN_NAME/"
