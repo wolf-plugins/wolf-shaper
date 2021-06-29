84a63f68f7f121dafbf9d0e2f6b3ed767af8756a
Patrick Desaulniers <patrick.desaulniers36@gmail.com>
2021-06-29 19:23:18 -0400

    Add format_code.sh script

---
+++ tools/format_code.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# This script formats all the cpp/hpp files in a specific directory using clang-format.
+
+set -e
+shopt -s globstar
+
+if [ $# -ne 1 ]; then
+	echo "Invalid number of arguments."
+	echo "Usage: format_code.sh <dir>"
+fi
+
+for cfile in "$1"/**/*.{cpp,hpp}; do
+	echo "Formatting $cfile"
+	clang-format -style=file -i "$cfile" 
+done
